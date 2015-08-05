0a56c085af8d7964cad6f1b6cc6edbf69a067770
sunchen115 <sunchen115work@gmail.com>
2015-08-05 13:14:25 +0800

    add plugin

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "sbt-rest-rpc"
+
+version := "1.0"
+
+scalaVersion := "2.11.4"
+
+sbtPlugin := true
---
+++ src/Hello.scala
@@ -0,0 +1,11 @@
+import sbt._
+import Keys._
+
+object HelloPlugin extends AutoPlugin {
+  override lazy val projectSettings = Seq(commands += helloCommand)
+  lazy val helloCommand =
+    Command.command("hello") { (state: State) =>
+      println("Hi!")
+      state
+    }
+}
